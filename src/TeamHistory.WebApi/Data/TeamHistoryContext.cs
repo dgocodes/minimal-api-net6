b92d97f39a88c9950d4086dd5ddaed65528705da
Diego Perillo <sis.dperillo@gmail.com>
2021-11-09 17:55:42 -0300

    Iniciando projeto

---
+++ src/TeamHistory.WebApi/Data/TeamHistoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamHistory.WebApi.Entities;
+
+namespace TeamHistory.WebApi.Data
+{
+    public class TeamHistoryContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("DataSource=TeamHistory.db;Cache=Shared");
+    }
+}
---
+++ src/TeamHistory.WebApi/Entities/Team.cs
@@ -0,0 +1,18 @@
+﻿namespace TeamHistory.WebApi.Entities
+{
+    public class Team
+    {
+        public Team(string name, DateTime foundationDate)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            FoundationDate = foundationDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Name { get; set; }
+
+        public DateTime FoundationDate { get; set; }
+    }
+}
